345103f3faff78b80a9a0819002c878808f00760
Baoxue-home <Blizzard_xc@126.com>
2017-07-05 23:46:15 +0800

    添加excel配置文件描述

---
+++ ConfigReader/Framework/ConfigImporter/Excel/Common/ExcelConfigInfo.cs
@@ -0,0 +1,40 @@
+﻿using Common.Config;
+using System.Collections.Generic;
+
+namespace ExcelImproter.Framework.ConfigImporter.Excel
+{
+    public interface INode{}
+
+    public class ConfigElementNodeInfo : INode
+    {
+        public int id;
+        public string name;
+        public string desc;
+        public string type;
+        public string rangeMin;
+        public string rangeMax;
+        public string refrenceConfigName;
+        public int refrenceConfigId;            
+    }
+    public class ConfigStructInfo : INode
+    {
+        public int Id;
+        public string name;
+        public string desc;
+        public List<ConfigElementNodeInfo> nodeInfoList;
+    }
+    public class ConfigNodeListInfo : INode
+    {
+        public int type;
+        public ConfigElementNodeInfo nodeInfo;
+    }
+    public class ConfigStructListInfo : INode
+    {
+        public int type;
+        public ConfigStructInfo structInfo;
+    }
+    public class ExcelConfigInfo : XmlConfigBase
+    {
+        public List<INode> nodeInfoList;
+    }
+}
